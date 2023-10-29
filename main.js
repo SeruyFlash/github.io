10475b0a27b487fba945e80098f12722c2bf0234
SeruyFlash <137722698+SeruyFlash@users.noreply.github.com>
2023-10-29 18:19:54 +0300

    Update the button

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-let bgcolor = '#000000'
-let changeColor = document.body.button
-changeColor.onclick = function(){
-    document.body.style.backgroundColor = bgcolor;
-}
+let ChangeBgColour = document.getElementById("ChangeBgColour");
+let WhiteBg = true;
+
+function changeBgClr() {
+      let body = document.body;
+      if (WhiteBg) {
+        body.style.backgroundColor = "darkgreen";
+      } else {
+        body.style.backgroundColor = "white";
+      }
+      WhiteBg = !WhiteBg;
+    }
+    ChangeBgColour.addEventListener("click", changeBgClr);
